7b68c43f0566e048343b2ab0058d5ea2c494a4db
asouza <alots.ssa@gmail.com>
2016-07-25 18:49:14 -0300

    montando a tabela com valores estaticos

--- src/main/webapp/js/app/autorComponents.js
+++ src/main/webapp/js/app/autorComponents.js
@@ -90,39 +90,28 @@ var AutorForm = React.createClass({
 
 
 var AutorTable = React.createClass({
+	getInitialState : function(){
+		return {lista :[{nome : "Rolinho", email :  "cp@email.com"}, {nome : "Tatuagem", email :  "cp@email.com"}]};
+	},
 	render: function(){
+		var autorNodes = this.state.lista.map(function(autor){
+			return(
+					<tr key={autor.nome}>
+						<td>{autor.nome}</td>
+						<td>{autor.email}</td>
+					</tr>
+				);
+			}); 
 	return(
 			<table className="pure-table">
 			<thead>
 				<tr>
-					<th>#</th>
-					<th>Make</th>
-					<th>Model</th>
-					<th>Year</th>
+					<th>Nome</th>
+					<th>email</th>
 				</tr>
 			</thead>
-
 			<tbody>
-				<tr>
-					<td>1</td>
-					<td>Honda</td>
-					<td>Accord</td>
-					<td>2009</td>
-				</tr>
-
-				<tr>
-					<td>2</td>
-					<td>Toyota</td>
-					<td>Camry</td>
-					<td>2012</td>
-				</tr>
-
-				<tr>
-					<td>3</td>
-					<td>Hyundai</td>
-					<td>Elantra</td>
-					<td>2010</td>
-				</tr>
+				{autorNodes}
 			</tbody>
 		</table>
 	);
