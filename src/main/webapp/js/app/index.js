debf21030b75d22bf36db0940fc86aa390942995
Shibao <marcio.shibao@caelum.com.br>
2016-08-03 17:57:07 -0300

    Criando uma pagina de entrada caso nao tenha nenhum outro componente selecionado

--- src/main/webapp/js/app/index.js
+++ src/main/webapp/js/app/index.js
@@ -2,10 +2,12 @@ import App from './App.js'
 import {Router, Route, browserHistory, IndexRoute} from '../react/ReactRouter.min.js'
 import {AutorBox} from './autorComponent.js'
 import {LivroBox} from './livroComponent.js'
+import {Home} from './Home.js'
 
 ReactDOM.render((
 	<Router history={browserHistory}>
     	<Route path="/index.html" component={App}>
+	    	<IndexRoute path="/index.html" component={{conteudo:Home}}/>
     		<Route path="/autor" component={{conteudo:AutorBox}}/>
     		<Route path="/livro" component={{conteudo:LivroBox}}/>
 		</Route>
