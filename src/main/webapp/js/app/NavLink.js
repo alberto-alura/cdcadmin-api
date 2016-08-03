5363e3367643fae44a2d1f8993f235d79185a0f5
Shibao <marcio.shibao@caelum.com.br>
2016-08-03 16:11:26 -0300

    Colocando tudo em um sidebar

--- src/main/webapp/js/app/NavLink.js
+++ src/main/webapp/js/app/NavLink.js
@@ -2,6 +2,6 @@ import { Link } from '../react/ReactRouter.min.js'
 
 export default React.createClass({
   render() {
-    return <Link {...this.props} activeClassName="active"/>
+    return <Link {...this.props} className="menu" activeClassName="active"/>
   }
 })
--- src/main/webapp/js/app/index.js
+++ src/main/webapp/js/app/index.js
@@ -3,12 +3,12 @@ import {Router, Route, browserHistory, IndexRoute} from '../react/ReactRouter.mi
 import {AutorBox} from './autorComponent.js'
 import {LivroBox} from './livroComponent.js'
 
-console.log(AutorBox);
 ReactDOM.render((
-  <Router history={browserHistory}>
-    <Route path="/index.html" component={App}>
-      <Route path="/autor" component={AutorBox}/>
-      <Route path="/livro" component={LivroBox}/>
-    </Route>
-  </Router>
-), document.getElementById('content'));
+	<Router history={browserHistory}>
+    	<Route path="/index.html" component={App}>
+    		<Route path="/autor" component={AutorBox}/>
+    		<Route path="/livro" component={LivroBox}/>
+		</Route>
+    </Router>
+), document.getElementById('menu'));
+
